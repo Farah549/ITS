a4d47dc4de815c361d474d00a248e1a8de9e3f7d
shahab akhtar <shahabakhtar170@outlook.com>
2020-02-24 10:41:43 +0500

    migration

---
+++ User.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ITS
+{
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+
+    }
+}
